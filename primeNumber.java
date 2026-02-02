9c848029b28463b41cda78692cc86fec78c3814f
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-02-03 00:51:30 +0530

    Prime number

---
+++ primeNumber.java
@@ -0,0 +1,20 @@
+public class primeNumber {
+    public static void main(String[] args) {
+        int num = 17;
+        boolean isPrime = true;
+
+        if(num<=1){
+            isPrime = false;
+        }
+
+        for(int i=2; i<=num/2; i++){
+            if(num%i==0){
+                isPrime = true;
+            }
+        }
+
+        if(isPrime){
+            System.out.println(num + " is a prime number");
+        }
+    }
+}

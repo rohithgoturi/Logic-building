6f64ce8eeac3e1583f693ce331a5a863fb8c1b83
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-02-02 23:53:52 +0530

    Fibonacci Series upto n

--- fibonacciSeries.java
+++ fibonacciSeries.java
@@ -1,5 +1,16 @@
 public class fibonacciSeries {
     public static void main(String[] args) {
+        int n = 10; // Number of terms in Fibonacci series
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: " + a + ", " + b);
+
+        for(int i = 2; i < n; i++){
+            int next = a + b;
+            System.out.print(", " + next);
+            a = b;
+            b = next;
+        }
         
     }
 }

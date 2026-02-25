7602b8dc1a46b87b1e059330e0a263a989921fe6
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-02-25 22:57:20 +0530

    done

--- swapping4.java
+++ swapping4.java
@@ -2,15 +2,11 @@ public class swapping4 {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-
-
         System.out.println("Before swapping a = "+a+", b = "+b);
 
         a = a ^ b;
         b = a ^ b;
         a = a ^ b;
-
-        
         System.out.println("After swapping a = "+a+", b = "+b);
     }
 }
